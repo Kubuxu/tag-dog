703dab9f43cbad13297416ff284b054b78a2e7a2
Jakub Sztandera <kubuxu@protonmail.ch>
2019-02-26 13:06:01 +0100

    Use gomod

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Kubuxu/tag-dog
+
+require (
+	github.com/apsdehal/go-logger v0.0.0-20180929234804-e1d9ff37593f
+	github.com/google/go-github/v24 v24.0.0
+	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20190220154721-9b3c75971fc9
+	gopkg.in/go-playground/webhooks.v5 v5.6.0
+)
--- index.go
+++ index.go
@@ -7,13 +7,16 @@ import (
 	"os"
 	"strings"
 
-	gapi "github.com/google/go-github/github"
+	"github.com/apsdehal/go-logger"
+	gapi "github.com/google/go-github/v24/github"
+	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 	"gopkg.in/go-playground/webhooks.v5/github"
 )
 
 var hook *github.Webhook
 var cli *gapi.Client
+var log *logger.Logger
 
 func init() {
 	var err error
@@ -21,6 +24,8 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+	log, _ := logger.New("tag-dog")
+	log.SetFormat("%{lvl}: %{message} %{file}:%{line}")
 
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
@@ -36,7 +41,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	payload, err := hook.Parse(r, github.PushEvent)
 	if err != nil {
 		if err == github.ErrEventNotFound {
-			fmt.Printf("hook.Parse: %s\n", err)
+			log.Noticef("hook.Parse: %s", err)
 			return
 		}
 	}
@@ -44,14 +49,22 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	switch payload.(type) {
 	case github.PushPayload:
 		push := payload.(github.PushPayload)
-		fmt.Printf("push: %+v\n", push)
 		if !strings.HasPrefix(push.Ref, tagPrefix) || !push.Created {
-			fmt.Printf("not tag push")
+			log.Debugf("not tag push: %s", push.Ref)
 		}
 		tag := strings.TrimPrefix(push.Ref, tagPrefix)
+		err := createIssue(tag, push)
+		if err != nil {
+			log.Errorf("creating issue: %s", err)
+		}
+	default:
+		log.Infof("unknown type: %+v\n", payload)
+	}
+}
 
-		title := fmt.Sprintf("Possibly erroneous tag pushed: %s", tag)
-		body := fmt.Sprintf(`Woof Woof :dog:
+func createIssue(tag string, push github.PushPayload) error {
+	title := fmt.Sprintf("Possibly erroneous tag pushed: %s", tag)
+	body := fmt.Sprintf(`Woof Woof :dog:
 
 @%s looks like you pushed old tag into this repo.
 Remove this tag by running `+"`git tag -d %s && git push origin :%[2]s`"+`
@@ -63,19 +76,15 @@ You can remove all of them in one go by running:
 Yours truly, with :poodle:, Tag Dog.
 `, push.Sender.Login, tag)
 
-		ir := &gapi.IssueRequest{
-			Title:     &title,
-			Body:      &body,
-			Assignees: &[]string{push.Sender.Login},
-		}
-		iss, resp, err := cli.Issues.Create(context.Background(), push.Repository.Owner.Login,
-			push.Repository.Name, ir)
-		if err != nil {
-			fmt.Printf("creating issue: %s\n", err)
-			return
-		}
-		fmt.Printf("created issue: %+v %+v\n", iss, resp)
-	default:
-		fmt.Printf("unknown type: %+v\n", payload)
+	ir := &gapi.IssueRequest{
+		Title:     &title,
+		Body:      &body,
+		Assignees: &[]string{push.Sender.Login},
 	}
+	_, _, err := cli.Issues.Create(context.Background(), push.Repository.Owner.Login,
+		push.Repository.Name, ir)
+	if err != nil {
+		return errors.Wrap(err, "creating issue")
+	}
+	return nil
 }
